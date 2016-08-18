c8510912cd5846a149c8acacd235601265e1df78
justin leggett <justin@codingrookie.com>
2016-08-18 08:33:28 -0500

    completed the assignment

--- chaining.js
+++ chaining.js
@@ -1,22 +1,9 @@
-/*Using one single line of JavaScript code, complete the following tasks on the array of integers below.
-
-Sort the numbers in descending order (10, 9, 8, 7, etc).
-Remove any integers greater than 19.
-Multiply each remaining number by 1.5 and then subtract 1.
-Then output (either in the DOM or the console) the sum of all the resulting numbers.*/
-
 
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-integers = integers.sort(function (a, b) {  return a - b;  }); //????
+var chained  = integers.sort(function (a, b) {  return b - a;  }).filter(function(int) { return int > 19; }).reduce(function(previousValue, currentValue, currentIndex) { if(currentIndex === 1) { previousValue = (previousValue * 1.5) - 1; } currentValue = (currentValue * 1.5) - 1; return previousValue + currentValue; });
+console.log(chained);
 
 
 
-console.log(integers);
 
-
-// This is an example of chaining methods
-// var rooms = "Master Suite|Living Room|Dining Room|Bathroom";
-// rooms = rooms.split( "|" ).sort().join( "," ).replace( /\,/g, " - " );
-
-// returns Bathroom - Dining Room - Living Room - Master Suite
